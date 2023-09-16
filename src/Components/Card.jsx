724e30944632de95fcc3e50506cd56d3f4ee9d1e
sakib ul alam <sakibalambulbul1@gmail.com>
2023-09-16 13:21:44 +0600

    edited readme file

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -12,9 +12,9 @@ const Card = ({carddata,handleaddcard}) => {
   <div className="card-body items-center text-center">
     <h2 className="card-title">{title}</h2>
     <p>{title_description}</p>
-    <div className='flex gap-5 items-center '>
-    <FaDollarSign></FaDollarSign><p> Price:{price}</p>
-        <FaBookOpen></FaBookOpen> <p> Credit: {credit}</p>
+    <div className='flex gap-2 items-center '>
+    <FaDollarSign></FaDollarSign><p>Price:{price}</p>
+        <FaBookOpen></FaBookOpen><p>Credit:{credit}hr</p>
     </div>
     <div className="card-actions">
       <button  onClick={()=>handleaddcard(carddata,id,credit)} className="btn btn-primary">Select</button>
